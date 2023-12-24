9438207b984bc62c145204f3b8cbf5e4c123969b
TIhara Jayawickrama <thihara.20200601@iit.ac.lk>
2023-12-24 11:04:49 +0530

    Initial Commit

---
+++ HelloCoreML/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloCoreML
+//
+//  Created by Tihara Jayawickrama on 2023-12-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloCoreML/HelloCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreMLApp.swift
+//  HelloCoreML
+//
+//  Created by Tihara Jayawickrama on 2023-12-24.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
